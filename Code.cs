d5a9a3122f4e6f7d054f7ea1a1e18bb6a78610f5
DivyaMishra6 <divyamishra599@gmail.com>
2023-06-23 00:20:02 +0530

    [Divya]: Add Element in tree

---
+++ Code.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinaryTreeAndHashTable
+{
+    public class Code<T> where T : IComparable<T>
+    {
+        public T Data { get; set; }
+        public Code<T> LeftTree { get; set; }
+        public Code<T> RightTree { get; set; }
+
+        public Code(T Data)
+        {
+            this.Data = Data;
+            this.LeftTree = null;
+            this.RightTree = null;
+        }
+
+        int leftcount = 0;
+        int rightcount = 0;
+
+        public void Add(T item)
+        {
+            T currentValue = this.Data;
+            if (currentValue.CompareTo(item) > 0)
+            {
+                if (LeftTree == null)
+                {
+                    this.LeftTree = new Code<T>(item);
+                }
+                else { this.LeftTree.Add(item); }
+            }
+
+            else
+            {
+                if (this.RightTree == null)
+                    this.RightTree = new Code<T>(item);
+                else
+                    this.RightTree.Add(item);
+            }
+        }
+
+
+        public void Display()
+        {
+            if (this.LeftTree != null)
+            {
+                this.leftcount++;
+                this.LeftTree.Display();
+            }
+            Console.WriteLine(this.Data.ToString());
+            if (this.RightTree != null)
+            {
+                this.rightcount++;
+                this.RightTree.Display();
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿namespace BinaryTreeAndHashTable
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Binary Search Tree And Hash Tree ");
+            Code<int> obj = new Code<int>(56);
+            obj.Add(30);
+            obj.Add(70);
+            obj.Display();
+        }
+    }
+}
